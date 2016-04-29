b34ed8370e4ebf66c117020d834d9282388fae39
akveeraj <anil.verma@rorko.com>
2016-04-29 10:36:58 +0000

    Transfer

---
+++ Application/Content/Template/tmp_footerlinks.pl
@@ -0,0 +1,10 @@
+<div id='footerlinks'>
+<a href='/propertiestorent/doc/' title='Properties to Rent'>Properties to Rent</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+<%'<a href='/register/account/' title='Register'>Register</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;%>
+<a href='/propertiesrequired/doc/' title='Properties Required'>Properties Required</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+<a href='/ourfees/doc/' title='Our Fees'>Our Fees</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+<a href='/depositscheme/doc/' title='Deposit Scheme'>Deposit Scheme</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+<a href='/otherservices/doc/' title='Other Services'>Other Services</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+<a href='/contact/doc/' title='Contact Us'>Contact Us</a>&nbsp;&nbsp;&middot;&nbsp;&nbsp;
+<a href='/gallery/doc/' title='Oxford Pictures'>Oxford Pictures</a>
+</div>
